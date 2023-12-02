1b08300df623294d264f941dd2f014b97f4eccb6
Mizuki <104298092+mizukitayama@users.noreply.github.com>
2023-12-03 00:24:04 +0900

    Second part of Day 1

--- day1/src/trebuchet.c
+++ day1/src/trebuchet.c
@@ -5,11 +5,33 @@
 #include <fcntl.h>
 #include "../get_next_line/get_next_line.h"
 
-bool is_digit(char c) {
-	if ('0' <= c && c <= '9') {
-		return true;
+typedef struct {
+    int number;
+    char *word;
+} number_word_pair;
+
+number_word_pair pairs[] = {
+	{1, "one"},
+	{2, "two"},
+	{3, "three"},
+	{4, "four"},
+	{5, "five"},
+	{6, "six"},
+	{7, "seven"},
+	{8, "eight"},
+	{9, "nine"}
+};
+
+int get_digit(char *str) {
+	if ('0' <= str[0] && str[0] <= '9') {
+			return str[0] - '0';
 	}
-	return false;
+	for (int j = 0; j < 9; j++) {
+		if (strncmp(&str[0], pairs[j].word, strlen(pairs[j].word)) == 0) {
+			return pairs[j].number;
+		}
+	}
+	return -1;
 }
 
 int get_number_in_str(char *str) {
@@ -17,14 +39,16 @@ int get_number_in_str(char *str) {
 	int num = 0;
 	size_t len = strlen(str);
 	for (size_t i = 0; i < len; i++) {
-		if (is_digit(str[i])) {
-			num += (str[i] - '0') * 10;
+		int digit = get_digit(&str[i]);
+		if (digit != -1) {
+			num += digit * 10;
 			break;
 		}
 	}
 	for (size_t j = len; j >= 0; j--) {
-		if (is_digit(str[j])) {
-			num += (str[j] - '0');
+		int digit = get_digit(&str[j]);
+		if (digit != -1) {
+			num += digit;
 			break;
 		}
 	}
