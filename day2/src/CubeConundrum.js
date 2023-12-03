452808ca6250dea54bc26a1170b123bbe90f637e
Mizuki <104298092+mizukitayama@users.noreply.github.com>
2023-12-03 16:07:41 +0900

    day 2: fix Part1

--- day2/src/CubeConundrum.js
+++ day2/src/CubeConundrum.js
@@ -1,6 +1,6 @@
 const CubeConundrum = (games) => {
   return games.reduce((acc, game, index) => {
-    return ( acc + (!game.substr(game.indexOf(":") + 2).split("; ").map(subset => {
+    return ( acc + (game.substr(game.indexOf(":") + 2).split("; ").map(subset => {
       return subset.split(", ").every((part) => {
         const [num, color] = part.split(" ")
         return (
@@ -9,7 +9,7 @@ const CubeConundrum = (games) => {
           (color === "blue" && parseInt(num) <= 14)
         )
       })
-    }).includes(false) ? index + 1 : 0))
+    }).includes(false) ? 0 : index + 1))
   }, 0)
 }
 
